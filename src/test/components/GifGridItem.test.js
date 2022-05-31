9541c704f68a3277b2a15f20216711f4f32bccc2
CaritoPava <carito9018@gmail.com>
2022-05-31 05:56:29 -0500

    Aplicacion para encontrar git

---
+++ src/test/components/GifGridItem.test.js
@@ -0,0 +1,9 @@
+import gifGridItem from "../../components/GifGridItem"
+import shallow from 'enzyme'
+
+describe('Pruebas en <GifGriditem />', () => {
+  test('debe de mostrar el componente correctamente', () => {
+
+  })
+
+})
